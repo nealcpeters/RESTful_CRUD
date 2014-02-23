c967b68d405040a9761599f5a8f7cafe5c3fa320
Neal Peters <nealcpeters@gmail.com>
2014-02-23 10:22:42 -0600

    final version

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -14,14 +14,41 @@ get '/read' do
 end
 
 get '/update' do
+	@notes = Note.all
 
 	erb :update
+end
+
+get '/delete' do
+	@notes = Note.all
+
+	erb :delete
+end
+
+post '/updated' do
+	@note = Note.find(params[:id])
+	@note.update_attributes(title: params[:title], content: params[:content])
+	erb :update_confirmation
+end
+
+post '/update' do
+	@note = Note.where(title: params[:title]).first
+
+	erb :update_note
 end
 
 post '/create' do
 	@note = Note.create(title: params[:title], content: params[:content])
 
 	redirect '/'
+end
+
+post '/delete' do
+	@note = Note.where(title: params[:title]).first
+
+	@note.destroy
+
+	erb :delete_confirmation
 end
 
 
